33d3435b3826709a8bfc1e0552d13ffc43aa147e
Pavel <pa11ady@yandex.ru>
2022-02-16 19:45:44 +0300

    feat

--- src/ru/yandex/practicum/tasktracker/taskmanager/TaskManager.java
+++ src/ru/yandex/practicum/tasktracker/taskmanager/TaskManager.java
@@ -5,16 +5,36 @@ import ru.yandex.practicum.tasktracker.task.Subtask;
 import ru.yandex.practicum.tasktracker.task.Task;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class TaskManager {
     private static long lastTaskId = 0;
+    private HashMap<Long, Task> taskHashMap = new HashMap<>();
     private HashMap<Long, Epic> epicHashMap = new HashMap<>();
     private HashMap<Long, Subtask> subtaskHashMap = new HashMap<>();
-    private HashMap<Long, Task> taskHashMap = new HashMap<>();
 
     public static long calcNextTaskId() {
         return ++lastTaskId;
     }
 
-    
+    //Получение списка всех задач
+    public List<Task> getList() {
+        return null;
+    }
+
+    //Удаление всех задач
+    public  void removeALL() {
+
+    }
+
+    //Получение по идентификатору
+
+    //Создание. Сам объект должен передаваться в качестве параметра
+
+    //Обновление. Новая версия объекта
+
+    //Удаление по идентификатору
+
+    //Получение списка всех подзадач определённого эпика
+
 }
