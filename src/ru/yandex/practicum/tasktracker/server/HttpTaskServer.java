95a085377291e7d2fc03b9d50ae6642779b8d184
Pavel <pa11ady@yandex.ru>
2022-05-11 19:38:33 +0300

    http

---
+++ src/ru/yandex/practicum/tasktracker/server/HttpTaskServer.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.tasktracker.server;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class HttpTaskServer {
+    private static final int PORT = 8080;
+    private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+
+    public void start() throws IOException {
+
+    }
+}
