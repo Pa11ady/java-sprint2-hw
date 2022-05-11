ddadde984cf715b43468a6f17e46a4839c2d1100
Pavel <pa11ady@yandex.ru>
2022-05-12 01:17:11 +0300

    HttpTaskServer прототип

--- src/ru/yandex/practicum/tasktracker/server/HttpTaskServer.java
+++ src/ru/yandex/practicum/tasktracker/server/HttpTaskServer.java
@@ -18,6 +18,12 @@ public class HttpTaskServer {
     public HttpTaskServer() throws IOException {
         httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
         httpServer.createContext("/test", this::handleTest);
+        httpServer.createContext("/tasks/task", this::handleTasksTask);
+        httpServer.createContext("/tasks/subtask", this::handleTasksSubtask);
+        httpServer.createContext("/tasks/epic", this::handleTasksEpic);
+        httpServer.createContext("/tasks/subtask/epic", this::handleTasksSubtaskEpic);
+        httpServer.createContext("/tasks/history", this::handleTasksHistory);
+        httpServer.createContext("/tasks", this::handleTasks);
     }
 
     public void start() {
@@ -28,6 +34,78 @@ public class HttpTaskServer {
     private void handleTest(HttpExchange httpExchange) throws IOException {
         System.out.println("handleTest");
         String response = "Привет, тестовый метод референс работает!";
+
+        Headers headers = httpExchange.getResponseHeaders();
+        headers.set("Content-Type", "text/html; charset=" + DEFAULT_CHARSET);
+        httpExchange.sendResponseHeaders(200, 0);
+
+        try (OutputStream os = httpExchange.getResponseBody()) {
+            os.write(response.getBytes(DEFAULT_CHARSET));
+        }
+    }
+
+    private void handleTasksTask(HttpExchange httpExchange) throws IOException {
+        String response = "handleTasksTask";
+
+        Headers headers = httpExchange.getResponseHeaders();
+        headers.set("Content-Type", "text/html; charset=" + DEFAULT_CHARSET);
+        httpExchange.sendResponseHeaders(200, 0);
+
+        try (OutputStream os = httpExchange.getResponseBody()) {
+            os.write(response.getBytes(DEFAULT_CHARSET));
+        }
+    }
+
+    private void handleTasksSubtask(HttpExchange httpExchange) throws IOException {
+        String response = "handleTasksSubtask";
+
+        Headers headers = httpExchange.getResponseHeaders();
+        headers.set("Content-Type", "text/html; charset=" + DEFAULT_CHARSET);
+        httpExchange.sendResponseHeaders(200, 0);
+
+        try (OutputStream os = httpExchange.getResponseBody()) {
+            os.write(response.getBytes(DEFAULT_CHARSET));
+        }
+    }
+
+    private void handleTasksEpic(HttpExchange httpExchange) throws IOException {
+        String response = "handleTasksEpic";
+
+        Headers headers = httpExchange.getResponseHeaders();
+        headers.set("Content-Type", "text/html; charset=" + DEFAULT_CHARSET);
+        httpExchange.sendResponseHeaders(200, 0);
+
+        try (OutputStream os = httpExchange.getResponseBody()) {
+            os.write(response.getBytes(DEFAULT_CHARSET));
+        }
+    }
+
+    private void handleTasksSubtaskEpic(HttpExchange httpExchange) throws IOException {
+        String response = "handleTasksSubtaskEpic";
+
+        Headers headers = httpExchange.getResponseHeaders();
+        headers.set("Content-Type", "text/html; charset=" + DEFAULT_CHARSET);
+        httpExchange.sendResponseHeaders(200, 0);
+
+        try (OutputStream os = httpExchange.getResponseBody()) {
+            os.write(response.getBytes(DEFAULT_CHARSET));
+        }
+    }
+
+    private void handleTasksHistory(HttpExchange httpExchange) throws IOException {
+        String response = "handleTasksHistory";
+
+        Headers headers = httpExchange.getResponseHeaders();
+        headers.set("Content-Type", "text/html; charset=" + DEFAULT_CHARSET);
+        httpExchange.sendResponseHeaders(200, 0);
+
+        try (OutputStream os = httpExchange.getResponseBody()) {
+            os.write(response.getBytes(DEFAULT_CHARSET));
+        }
+    }
+
+    private void handleTasks(HttpExchange httpExchange) throws IOException {
+        String response = "handleTasks";
 
         Headers headers = httpExchange.getResponseHeaders();
         headers.set("Content-Type", "text/html; charset=" + DEFAULT_CHARSET);
