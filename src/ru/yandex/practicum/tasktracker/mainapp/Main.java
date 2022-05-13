8bb0422ba58f9ad454a17fddc57a9c235b538fab
Pavel <pa11ady@yandex.ru>
2022-05-13 17:52:19 +0300

    main

--- src/ru/yandex/practicum/tasktracker/mainapp/Main.java
+++ src/ru/yandex/practicum/tasktracker/mainapp/Main.java
@@ -10,12 +10,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         new HttpTaskServer().start();
-        //getAllTasks();
+        createTask(new Task(100L, "task100", "task100", TaskStatus.NEW));
+        createTask(new Task(200L, "tas200", "task200", TaskStatus.NEW));
+        createTask(new Task(200L, "tas300", "task300", TaskStatus.NEW));
+        getAllTasks();
+        System.out.println("********");
+        getTask(100L);
     }
 
     private static void getAllTasks() throws IOException, InterruptedException {
@@ -23,10 +30,16 @@ public class Main {
         URI url = URI.create("http://localhost:8080/tasks/task/");
         HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        Gson gson = new Gson();
+        System.out.println("=======");
+        Task[] tasks = gson.fromJson(response.body(), Task[].class);
+        for(var el : tasks) {
+            System.out.println(el);
+        }
     }
 
-    private static void createTask() throws IOException, InterruptedException {
-        Task newTask = new Task("task1", "task1", TaskStatus.NEW);
+    private static void createTask(Task newTask) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         URI url = URI.create("http://localhost:8080/tasks/task/");
         Gson gson = new Gson();
@@ -36,10 +49,14 @@ public class Main {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 
-    private static void getTask() throws  IOException, InterruptedException{
+    private static void getTask(Long id) throws  IOException, InterruptedException{
         HttpClient client = HttpClient.newHttpClient();
-        URI url = URI.create("http://localhost:8080/tasks/task/?id=1");
+        URI url = URI.create("http://localhost:8080/tasks/task/?id=" + id);
         HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        Gson gson = new Gson();
+        Task task = gson.fromJson(response.body(), Task.class);
+        System.out.println(task);
     }
 }
